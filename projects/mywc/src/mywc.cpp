8b261af5e8bff94e16b317e3715b597cf35e0736
Jashwanth12707 <a.jashwanth0111@gmail.com>
2026-07-23 08:08:16 +0000

    Implement mywc utility

---
+++ projects/mywc/src/mywc.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fcntl.h>
+#include <cstring>
+#include <unistd.h>
+#include <cerrno>
+
+using namespace std;
+
+int main(int argc,char*argv[]){
+
+    if (argc!=2){
+        cerr<<"Usage : wc <filename\n";
+        return 1;
+    }
+    
+    int fd=open(argv[1],O_RDONLY);
+    
+    if( fd ==-1){
+        perror("open");
+        return 1;
+    }
+    char buff[4096];
+    ssize_t buffsize;
+    int bytes=0;
+    int lines=0;
+    int words=0;
+    bool inword=false;
+
+    while((buffsize = read(fd, buff, sizeof(buff))) > 0){
+        bytes+=buffsize;
+
+        for(int i=0;i<buffsize;i++){
+
+            char ch =buff[i];
+            
+            if (ch == ' ' || ch == '\n' || ch == '\t')
+            {
+                inword = false;
+            }
+            else if (!inword)
+            {
+                words++;
+                inword = true;
+            }
+            if(ch=='\n'){
+                lines++;
+            }
+        }
+    }
+    close(fd);
+cout<<lines<<" "
+    <<words<<" "
+    <<bytes<<"\n";
+
+
+
+}
